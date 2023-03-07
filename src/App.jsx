59db4f6265f7b54b22ba21faf6bcb2281cfdecdf
minhlong149 <minhlong9514@gmail.com>
2023-03-07 15:43:41 +0700

    Init project

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+import Blog from './components/Blog';
+import blogService from './services/blogs';
+
+function App() {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
+
+  return (
+    <div>
+      <h2>blogs</h2>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
+    </div>
+  );
+}
+
+export default App;
