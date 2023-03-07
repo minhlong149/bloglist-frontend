a9c6439f82b089d8240c3c5aebe097da84de1073
minhlong149 <minhlong9514@gmail.com>
2023-03-07 17:45:03 +0700

    Allow a logged-in user to add new blogs

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import blogService from './services/blogs';
 import loginService from './services/login';
 
 function App() {
+  const [newTitle, setNewTitle] = useState('');
+  const [newAuthor, setNewAuthor] = useState('');
+  const [newURL, setNewURL] = useState('');
+
   const [blogs, setBlogs] = useState([]);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -29,17 +33,36 @@ function App() {
   const handleLogin = async (event) => {
     event.preventDefault();
     try {
-      // The token returned with a successful login is saved to the application's state user.
       const user = await loginService.login({
         username,
         password,
       });
       setUser(user);
+      blogService.setToken(user.token);
       setUsername('');
       setPassword('');
       window.localStorage.setItem('loggedUser', JSON.stringify(user));
     } catch (exception) {
       console.log('Wrong credentials');
+    }
+  };
+
+  const addBlog = async (event) => {
+    event.preventDefault();
+    try {
+      const newBlog = {
+        title: newTitle,
+        author: newAuthor,
+        url: newURL,
+      };
+      const returnedBlog = await blogService.create(newBlog);
+      setBlogs(blogs.concat(returnedBlog));
+      setNewTitle('');
+      setNewAuthor('');
+      setNewURL('');
+      window.localStorage.setItem('loggedUser', JSON.stringify(user));
+    } catch (error) {
+      console.log(error);
     }
   };
 
@@ -70,11 +93,45 @@ function App() {
     </div>
   );
 
+  const BlogForm = (
+    <form onSubmit={addBlog}>
+      <div>
+        <label htmlFor="title">Title: </label>
+        <input
+          type="text"
+          name="title"
+          value={newTitle}
+          onChange={({ target }) => setNewTitle(target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="author">Author: </label>
+        <input
+          type="text"
+          name="author"
+          value={newAuthor}
+          onChange={({ target }) => setNewAuthor(target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="author">Url: </label>
+        <input
+          type="text"
+          name="author"
+          value={newURL}
+          onChange={({ target }) => setNewURL(target.value)}
+        />
+      </div>
+      <input type="submit" value="Create"></input>
+    </form>
+  );
+
   const Blogs = (
     <div>
       <h2>blogs</h2>
       <button onClick={() => logOut()}>Log out</button>
       <p>{user?.name} logged in</p>
+      {BlogForm}
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
